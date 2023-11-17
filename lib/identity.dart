072acd8119e884b0f77fc818c08714a4046157d9
Nick Meinhold <nick@enspyr.co>
2023-11-17 19:09:42 +1100

    Add IdentitySubsystem

--- lib/identity.dart
+++ lib/identity.dart
@@ -6,10 +6,39 @@ abstract class IdentityConcept {
   abstract final IdentityBeliefs identity;
 }
 
+abstract interface class IdentitySubsystem {
+  Future<String?>? getCurrentIdToken();
+  String? getCurrentUserId();
+  Future<Set<ProviderEnum>> retrieveProvidersFor(String email);
+  Stream<UserAuthState> get onAuthStateChange;
+  Future<UserAuthState> signInAnonymously();
+  Future<UserAuthState> signUpWithEmailAndPassword(
+      String email, String password);
+  Future<UserAuthState> signInWithEmailAndPassword(
+      String email, String password);
+  Future<UserAuthState> signInWithApple(
+      {required String idToken, required String rawNonce});
+  Future<UserAuthState> signInWithGoogle(
+      {required String accessToken, required String idToken});
+  Future<void> signOut();
+  Future<UserAuthState> linkGoogleAccount(
+      {required String accessToken, required String idToken});
+  Future<UserAuthState> linkAppleAccount(
+      {required String idToken, required String rawNonce});
+}
+
 enum IdentityProvider {
   apple,
   google,
   github,
+}
+
+sealed class ProviderCredential {}
+
+class AppleCredential implements ProviderCredential {
+  AppleCredential(this.idToken, this.rawNonce);
+  final String idToken;
+  final String rawNonce;
 }
 
 abstract class IdentityBeliefs implements CoreBeliefs {
